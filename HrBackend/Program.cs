79472e25c3dfa3a51cfb0ece86c124c86c87c91d
Xin Ye Yang <Xin_Ye_Yang@ose.com.tw>
2026-01-05 16:39:41 +0800

    Backend: Link HRContext to Program.cs and update connection string

--- HrBackend/Program.cs
+++ HrBackend/Program.cs
@@ -1,3 +1,5 @@
+using HrBackend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HrBackend
 {
@@ -8,7 +10,10 @@ namespace HrBackend
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            //註冊SQL連線服務
+            builder.Services.AddDbContext<HRContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //註冊controller服務
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
