ad1a301826a24c842e0f23ab419285a10b94ba7e
Xin Ye Yang <Xin_Ye_Yang@ose.com.tw>
2026-01-08 13:23:27 +0800

    20260108

---
+++ HrBackend/DTOs/UpdateProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HrBackend.DTOs
+{
+    public class UpdateProfileDto
+    {
+        public string? Email { get; set; } 
+        public string? Phone { get; set; }
+        public string? EmergencyContact { get; set; }
+    }
+}
